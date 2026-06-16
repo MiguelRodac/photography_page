2ba26a2e1f06abc23fdaf0ffdc94397de6f9d661
Miguel Rodac <rodacmig19@gmail.com>
2026-06-16 00:04:50 -0400

    feat: add environment configuration files for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  siteName: 'Photography ACAS',
+  siteUrl: 'https://www.photographyacas.com',
+  socialMedia: {
+    instagram: 'https://www.instagram.com/',
+    whatsapp: 'https://wa.me/5491157445478',
+    facebook: '',
+  },
+  contact: {
+    email: '',
+    phone: '',
+    whatsapp: '5491157445478',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  siteName: 'Photography ACAS',
+  siteUrl: 'http://localhost:4200',
+  socialMedia: {
+    instagram: 'https://www.instagram.com/',
+    whatsapp: 'https://wa.me/5491157445478',
+    facebook: '',
+  },
+  contact: {
+    email: '',
+    phone: '',
+    whatsapp: '5491157445478',
+  },
+};
