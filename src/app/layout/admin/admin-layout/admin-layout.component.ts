1246e19d858b08341eb896800f507abf7b0531f4
Miguel Rodac <rodacmig19@gmail.com>
2026-06-22 22:58:36 -0400

    fix(admin): remove unused HeaderAdmin/FooterAdmin imports and unnecessary optional chains

--- src/app/layout/admin/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin/admin-layout/admin-layout.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderAdminComponent } from "../header-admin/header-admin.component";
-import { FooterAdminComponent } from "../footer-admin/footer-admin.component";
 import { NavBarAdminComponent } from "../nav-bar-admin/nav-bar-admin.component";
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, HeaderAdminComponent, FooterAdminComponent, NavBarAdminComponent],
+  imports: [RouterOutlet, NavBarAdminComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
